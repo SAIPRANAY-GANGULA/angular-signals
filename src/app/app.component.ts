d9f442b3185d788261d5899d9d3c891f4044d48a
SAIPRANAY-GANGULA <gangulasaipranay@gmail.com>
2023-03-20 17:14:29 +0530

    code with signals :)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ` <p>Search string: {{ search }}</p>
+  template: ` <p>Search string: {{ search() }}</p>
     <input type="text" (input)="setSearchString($event)" />
     <hr />
-    <p *ngFor="let user of filteredUsers">{{ user.id }}. {{ user.name }}</p>
+    <p *ngFor="let user of filteredUsers()">{{ user.id }}. {{ user.name }}</p>
     <hr />
     <button (click)="addUser()">Add Random User</button>`,
 })
-export class AppComponent implements OnInit {
-  search = '';
-  filteredUsers: Array<{ id: number; name: string }> = [];
-  private users = [
+export class AppComponent {
+  search = signal('');
+  filteredUsers = computed(() =>
+    this.users().filter((user) =>
+      user.name.toLowerCase().startsWith(this.search().toLowerCase())
+    )
+  );
+  private users = signal([
     { id: 1, name: 'Sai' },
     { id: 2, name: 'Pranay' },
     { id: 3, name: 'Gangula' },
-  ];
-
-  ngOnInit(): void {
-    this.filteredUsers = this.users;
-  }
+  ]);
 
   setSearchString(e: Event): void {
-    this.search = (e.target as HTMLInputElement).value;
-    this.filteredUsers = this.users.filter((user) =>
-      user.name.toLowerCase().startsWith(this.search.toLowerCase())
-    );
+    this.search.set((e.target as HTMLInputElement).value);
   }
 
   addUser(): void {
-    this.users = [
-      ...this.users,
-      { id: this.users.length + 1, name: 'Ankitha' },
-    ];
-    this.filteredUsers = this.users;
+    this.users.update((users) => [
+      ...users,
+      { id: this.users().length + 1, name: 'Ankitha' },
+    ]);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule, {
+    // ngZone: 'noop', // CD doesn't work if we disable Zone.js
+    // Currently, signals work in the context of ZoneJs. A new CD mechanism will come in a bit later.
+  })
+  .catch((err) => console.error(err));
