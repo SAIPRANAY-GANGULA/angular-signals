93744bb61fa49de7f266d41c14664ad9fe0de3be
SAIPRANAY-GANGULA <gangulasaipranay@gmail.com>
2023-03-20 16:23:10 +0530

    code without signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: ` <p>Search string: {{ search }}</p>
     <input type="text" (input)="setSearchString($event)" />
     <hr />
-    <ul>
-      <li *ngFor="let user of filteredUsers">{{ user.name }}</li>
-    </ul>
+    <p *ngFor="let user of filteredUsers">{{ user.id }}. {{ user.name }}</p>
     <hr />
     <button (click)="addUser()">Add Random User</button>`,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   search = '';
-  users = [
+  filteredUsers: Array<{ id: number; name: string }> = [];
+  private users = [
     { id: 1, name: 'Sai' },
     { id: 2, name: 'Pranay' },
     { id: 3, name: 'Gangula' },
   ];
 
-  filteredUsers = this.users;
+  ngOnInit(): void {
+    this.filteredUsers = this.users;
+  }
 
-  setSearchString(e: Event) {
+  setSearchString(e: Event): void {
     this.search = (e.target as HTMLInputElement).value;
     this.filteredUsers = this.users.filter((user) =>
       user.name.toLowerCase().startsWith(this.search.toLowerCase())
     );
   }
 
-  addUser() {
-    this.users = [...this.users, { id: 3, name: 'John' }];
+  addUser(): void {
+    this.users = [
+      ...this.users,
+      { id: this.users.length + 1, name: 'Ankitha' },
+    ];
+    this.filteredUsers = this.users;
   }
 }
