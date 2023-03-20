fb32a28da70787c7e09324551a9d54f4c18f3edb
SAIPRANAY-GANGULA <gangulasaipranay@gmail.com>
2023-03-20 16:15:11 +0530

    code without signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,33 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: ` <p>Search string: {{ search }}</p>
+    <input type="text" (input)="setSearchString($event)" />
+    <hr />
+    <ul>
+      <li *ngFor="let user of filteredUsers">{{ user.name }}</li>
+    </ul>
+    <hr />
+    <button (click)="addUser()">Add Random User</button>`,
 })
 export class AppComponent {
-  title = 'signals';
+  search = '';
+  users = [
+    { id: 1, name: 'Sai' },
+    { id: 2, name: 'Pranay' },
+    { id: 3, name: 'Gangula' },
+  ];
+
+  filteredUsers = this.users;
+
+  setSearchString(e: Event) {
+    this.search = (e.target as HTMLInputElement).value;
+    this.filteredUsers = this.users.filter((user) =>
+      user.name.toLowerCase().startsWith(this.search.toLowerCase())
+    );
+  }
+
+  addUser() {
+    this.users = [...this.users, { id: 3, name: 'John' }];
+  }
 }
